e6b7cc3ca48186c71fdf65224196756e0a92d892
JD17VM <jdiegodiego4@gmail.com>
2025-09-27 07:52:19 -0500

    :test_tube: feat: implement local monte carlo simulation

--- lab-2-programming-assignments/ejercicio_3_2.cpp
+++ lab-2-programming-assignments/ejercicio_3_2.cpp
@@ -11,6 +11,30 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
+    long long number_of_tosses = 100000;
+
+    if (my_rank == 0) {
+        cout << "Usando " << number_of_tosses << " lanzamientos totales." << endl;
+    }
+
+    MPI_Bcast(&number_of_tosses, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
+
+    long long local_number_of_tosses = number_of_tosses / comm_sz;
+    long long number_in_circle = 0;
+
+    random_device rd;
+    mt19937 gen(rd() + my_rank);
+    uniform_real_distribution<> dis(-1.0, 1.0);
+
+    for (long long toss = 0; toss < local_number_of_tosses; toss++) {
+        double x = dis(gen);
+        double y = dis(gen);
+        double distance_squared = x * x + y * y;
+        if (distance_squared <= 1) {
+            number_in_circle++;
+        }
+    }
+
     MPI_Finalize();
     return 0;
 }
