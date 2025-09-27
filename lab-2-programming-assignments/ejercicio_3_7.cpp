98a84faf564e44305f7040328e858a16e94091c1
JD17VM <jdiegodiego4@gmail.com>
2025-09-27 08:52:37 -0500

    :ping_pong: feat: add ping-pong communication loop and timing measurement

--- lab-2-programming-assignments/ejercicio_3_7.cpp
+++ lab-2-programming-assignments/ejercicio_3_7.cpp
@@ -23,6 +23,33 @@ int main(int argc, char** argv) {
     const int MSG_SIZE = 1; 
     vector<int> msg(MSG_SIZE, 0);
 
+    double mpi_wtime_start, mpi_wtime_finish;
+    clock_t c_start, c_finish;
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    if (my_rank == 0) {
+        mpi_wtime_start = MPI_Wtime();
+        c_start = clock();
+
+        for (int i = 0; i < PING_PONG_LIMIT; ++i) {
+            MPI_Send(msg.data(), MSG_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD);
+            MPI_Recv(msg.data(), MSG_SIZE, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+
+        mpi_wtime_finish = MPI_Wtime();
+        c_finish = clock();
+        
+        cout << "Tiempo total (MPI_Wtime): " << (mpi_wtime_finish - mpi_wtime_start) << " s" << endl;
+        cout << "Tiempo total (clock): " << ((double)(c_finish - c_start) / CLOCKS_PER_SEC) << " s" << endl;
+
+    } else if (my_rank == 1) {
+        for (int i = 0; i < PING_PONG_LIMIT; ++i) {
+            MPI_Recv(msg.data(), MSG_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(msg.data(), MSG_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+    }
+
     MPI_Finalize();
     return 0;
 }
