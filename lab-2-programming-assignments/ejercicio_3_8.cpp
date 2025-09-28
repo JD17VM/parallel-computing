358d8c0c98035f80e956982403ca8b7192737923
JD17VM <jdiegodiego4@gmail.com>
2025-09-28 16:59:07 -0500

    :sparkles: feat: add local sort for each process's key vector

--- lab-2-programming-assignments/ejercicio_3_8.cpp
+++ lab-2-programming-assignments/ejercicio_3_8.cpp
@@ -6,13 +6,6 @@
 
 using namespace std;
 
-vector<int> merge(const vector<int>& a, const vector<int>& b) {
-    vector<int> result;
-    result.reserve(a.size() + b.size());
-    merge(a.begin(), a.end(), b.begin(), b.end(), back_inserter(result));
-    return result;
-}
-
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
     int comm_sz, my_rank;
@@ -33,28 +26,6 @@ int main(int argc, char** argv) {
 
     sort(local_keys.begin(), local_keys.end());
     
-    for (int i = 1; i < comm_sz; i *= 2) {
-        if (my_rank % (2 * i) == 0) {
-            if (my_rank + i < comm_sz) {
-                int partner_size;
-                MPI_Status status;
-                MPI_Probe(my_rank + i, 0, MPI_COMM_WORLD, &status);
-                MPI_Get_count(&status, MPI_INT, &partner_size);
-
-                vector<int> partner_keys(partner_size);
-                MPI_Recv(partner_keys.data(), partner_size, MPI_INT, my_rank + i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                local_keys = merge(local_keys, partner_keys);
-            }
-        } else if (my_rank % i == 0) {
-            MPI_Send(local_keys.data(), local_keys.size(), MPI_INT, my_rank - i, 0, MPI_COMM_WORLD);
-            break; 
-        }
-    }
-
-    if (my_rank == 0) {
-        cout << "Ordenamiento por mezcla paralelo completado." << endl;
-    }
-
     MPI_Finalize();
     return 0;
 }
