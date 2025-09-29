255e3f06f4585fdf6e00086ad6a1a74b4c206136
JD17VM <jdiegodiego4@gmail.com>
2025-09-28 19:34:16 -0500

    :sparkles: feat: add cyclic-to-block data redistribution

--- lab-2-programming-assignments/ejercicio_3_9.cpp
+++ lab-2-programming-assignments/ejercicio_3_9.cpp
@@ -25,7 +25,6 @@ int main(int argc, char** argv) {
     vector<int> send_buf_b2c;
     for(int i = 0; i < local_n; ++i) {
         if ((block_data[i] % comm_sz) == my_rank) {
-            // Keep it
         } else {
             send_buf_b2c.push_back(block_data[i]);
         }
@@ -34,9 +33,29 @@ int main(int argc, char** argv) {
     MPI_Alltoall(send_buf_b2c.data(), send_buf_b2c.size()/comm_sz, MPI_INT, 
                  cyclic_data.data(), local_n/comm_sz, MPI_INT, MPI_COMM_WORLD);
 
+
     double end_time = MPI_Wtime();
     if (my_rank == 0) {
         cout << "Tiempo de Bloque -> Cíclico: " << end_time - start_time << " s" << endl;
+    }
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    start_time = MPI_Wtime();
+
+    vector<int> send_buf_c2b;
+    for(int i = 0; i < local_n; ++i){
+        if( (cyclic_data[i] / local_n) == my_rank){
+        } else {
+            send_buf_c2b.push_back(cyclic_data[i]);
+        }
+    }
+    
+    MPI_Alltoall(send_buf_c2b.data(), send_buf_c2b.size()/comm_sz, MPI_INT, 
+                 block_data.data(), local_n/comm_sz, MPI_INT, MPI_COMM_WORLD);
+
+    end_time = MPI_Wtime();
+    if (my_rank == 0) {
+        cout << "Tiempo de Cíclico -> Bloque: " << end_time - start_time << " s" << endl;
     }
 
     MPI_Finalize();
