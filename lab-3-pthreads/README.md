114b418a5fa57d125945fc3ede63836f76df5c92
JD17VM <jdiegodiego4@gmail.com>
2025-10-06 19:49:12 -0500

    feat: :lock: add pi calculation using mutex for thread-safe accumulation

--- lab-3-pthreads/README.md
+++ lab-3-pthreads/README.md
@@ -5,7 +5,7 @@ Implementar el cálculo de PI usando las estratégias:
 ✔️ Secuencial,
 ✔️ Busy-Waiting  dentro del bucle FOR
 ✔️ Busy-Waiting  fuera del bucle FOR
-- MUTEX
+✔️ MUTEX
 
 Comparar y analizar los resultados de todas las estratégias.
 
--- lab-3-pthreads/laboratorio_pi.cpp
+++ lab-3-pthreads/laboratorio_pi.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <mutex>
 #include <chrono>
 #include <iomanip>
 
@@ -15,6 +16,8 @@ const int num_threads = 4;
 double sum = 0.0;
 
 volatile int flag = 0;
+
+mutex mtx;
 
 
 void calculate_pi_sequential() {
@@ -104,6 +107,42 @@ void calculate_pi_busy_outside() {
 }
 
 
+// --- 4. MUTEX ---
+void worker_mutex(long long start, long long end, double step) {
+    double local_sum = 0.0;
+
+    for (long long i = start; i < end; ++i) {
+        double x = (i + 0.5) * step;
+        local_sum += 4.0 / (1.0 + x * x);
+    }
+
+    lock_guard<mutex> guard(mtx);
+    sum += local_sum;
+}
+
+void calculate_pi_mutex() {
+    sum = 0.0;
+    double step = 1.0 / num_steps;
+    vector<thread> threads;
+    long long steps_per_thread = num_steps / num_threads;
+
+    for (int i = 0; i < num_threads; ++i) {
+        long long start = i * steps_per_thread;
+        long long end = (i == num_threads - 1) ? num_steps : start + steps_per_thread;
+        threads.emplace_back(worker_mutex, start, end, step);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+
+    double pi = sum * step;
+    cout << "  PI (Mutex): " << setprecision(15) << pi << endl;
+}
+
+
+
+
 int main() {
     cout << "Calculando PI con " << num_steps << " pasos y " << num_threads << " hilos." << endl;
 
@@ -121,6 +160,12 @@ int main() {
 
     start_time = high_resolution_clock::now();
     calculate_pi_busy_outside();
+    end_time = high_resolution_clock::now();
+    diff = end_time - start_time;
+    cout << "  Tiempo: " << diff.count() << " s\n" << endl;
+
+    start_time = high_resolution_clock::now();
+    calculate_pi_mutex();
     end_time = high_resolution_clock::now();
     diff = end_time - start_time;
     cout << "  Tiempo: " << diff.count() << " s\n" << endl;
