70b39fc60979d8c79b7c0b2bf59cf376979c344f
JD17VM <jdiegodiego4@gmail.com>
2025-10-12 11:28:06 -0500

    feat: :mag: add thread-safe member operation using read lock

--- lab-4-pthreads/linked_list.cpp
+++ lab-4-pthreads/linked_list.cpp
@@ -24,8 +24,35 @@ void ThreadSafeList::Destroy() {
     head = nullptr;
 }
 
-void ThreadSafeList::Init(int initial_size) { }
-bool ThreadSafeList::Member(int value) { return false; }
+void ThreadSafeList::Destroy() {
+    Node* current = head;
+    while (current != nullptr) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+    head = nullptr;
+}
+
+void ThreadSafeList::Init(int initial_size) {
+    for (int i = 0; i < initial_size; ++i) {
+    }
+}
+
+bool ThreadSafeList::Member(int value) {
+    pthread_rwlock_rdlock(&list_rwlock);
+    Node* current = head;
+    while (current != nullptr) {
+        if (current->data == value) {
+            pthread_rwlock_unlock(&list_rwlock);
+            return true;
+        }
+        current = current->next;
+    }
+    pthread_rwlock_unlock(&list_rwlock);
+    return false;
+}
+
 bool ThreadSafeList::Insert(int value) { return false; }
 bool ThreadSafeList::Delete(int value) { return false; }
-void ThreadSafeList::Print() { }
+void ThreadSafeList::Print() {}
